5902ecc04f9f69c8db218c7ad642b2bc3e4ddc7a
LaoBodsl <laobodslr@gmail.com>
2019-07-27 16:48:51 +0800

    spit

---
+++ tensquare_spit/src/main/java/com/tensquare/spit/controller/SpitController.java
@@ -0,0 +1,53 @@
+package com.tensquare.spit.controller;
+
+
+import com.tensquare.spit.pojo.Spit;
+import com.tensquare.spit.service.SpitService;
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+@CrossOrigin
+
+@RequestMapping("/spit")
+public class SpitController {
+
+    @Autowired
+    private SpitService spitService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Result findAll(){
+        return new Result(true, StatusCode.OK,"查询成功",spitService.findAll());
+    }
+
+    @RequestMapping(value = "/{spitId}", method = RequestMethod.GET)
+    public Result findById(@PathVariable String spitId){
+        return new Result(true,StatusCode.OK,"查询成功",spitService.findById(spitId));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Result add(@RequestBody Spit spit){
+        spitService.save(spit);
+        return new Result(true,StatusCode.OK,"增加成功");
+    }
+
+
+    @RequestMapping(value = "/{spitId}", method = RequestMethod.POST)
+    public Result update(@RequestBody Spit spit, @PathVariable String spitId){
+        spit.set_id(spitId);
+        spitService.update(spit);
+        return new Result(true,StatusCode.OK,"修改成功");
+    }
+
+
+    @RequestMapping(value = "/{spitId}", method = RequestMethod.DELETE)
+    public Result deleteById(@PathVariable String spitId){
+        spitService.deleteById(spitId);
+        return new Result(true,StatusCode.OK,"删除成功");
+    }
+
+}
